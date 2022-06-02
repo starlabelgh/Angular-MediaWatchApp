b02e0210c2307a9ed1703db8e867898179881e71
starlabelgh <hello@starlabel.co>
2022-06-02 23:25:44 +0000

    update on the media item

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import {Component} from 'angular2/core';
+import {MediaItemComponent} from './media-item.component';
 
 @Component({
-    selector: 'app'
-    template: '<h1>My App</h1>'
+    selector: 'media-tracker-app',
+    directives: [MediaItemComponent],
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 export class AppComponent {}
